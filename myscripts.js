08bad8b20cee831a80b5da6c39f099291fbc15c6
rohkushw <rohit.kushwaha@capgemini.com>
2018-04-10 17:41:10 +0530

    Little edited Javascript

--- myscripts.js
+++ myscripts.js
@@ -1,4 +1,5 @@
 function myfunction(){
     document.getElementById("p01").innerHTML = "You Don't deserve any discount";
     document.getElementById("p01").style.color = "red";
+    document.getElementById("b01").style.visibility = "hidden";
 }
