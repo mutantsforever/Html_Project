74f1a45dcaf67847a91a36484486c0e1db1f511a
Kushwaha <rohit.kushwaha@capgemini.com>
2018-07-09 14:03:00 +0530

    Final commmit

--- myscripts.js
+++ myscripts.js
@@ -3,7 +3,7 @@ function myfunction(){
     document.getElementById("p01").innerHTML = "You Don't deserve any discount";
     document.getElementById("p01").style.color = "red";
     document.getElementById("b01").style.visibility = "hidden";
-}
+};
 function myMap() {
     var mapOptions = {
         center: new google.maps.LatLng(20.5937, 78.9629), 
@@ -11,7 +11,7 @@ function myMap() {
         mapTypeId: google.maps.MapTypeId.ROADMAP
     }
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-}
+};
 
 function sayHello(){
     $("#title").html('Hello');
@@ -21,13 +21,14 @@ function sayHello(){
 
     })
 };
-var paused = false;
-function swap(){
+
+function chng(){
+    var paused = false;
 $(".crop-img").click(function(){
     $("#bigImage").attr('src', 
         $(this).attr('src'));
 })
-};
+
 
 // var counter = 0;
 // $("#number").text(counter);
@@ -36,8 +37,10 @@ $(".crop-img").click(function(){
 //     $("#number").text(counter);
 // };
 
+
 var counter = 1;
 $("#image" + counter).click();
+
 $("#backward").click(function (){
     counter = counter - 1;
     if(counter < 1){
@@ -50,13 +53,17 @@ $("#forward").click(function (){
     if(counter > 4){
 		counter = 1;
     }
+
     $("#image" + counter).click();
 });
+
 $("#bigImage").click(function (){
 	paused = !paused;
     });
+
 setInterval(function (){
     if(!paused){
         $("#forward").click();
 	};
-}, 3000);
+}, 5000);
+};
